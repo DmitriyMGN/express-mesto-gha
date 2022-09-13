868ca8407ddec8fb77bf6bfc7bc34c81ee5b0add
Dmitriy <dmitry.mysyagin@yandex.ru>
2022-09-13 13:22:22 +0300

    fix 7

--- controllers/users.js
+++ controllers/users.js
@@ -111,7 +111,7 @@ const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ email }).select('+password');
     if (!user) {
-      return next(new NotFoundError('Такого пользователя не существует'));
+      return next(new NotFoundError('Такого пользователя не существует.'));
     }
 
     const isUserValid = await bcrypt.compare(password, user.password);
